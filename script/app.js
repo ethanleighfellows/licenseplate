3f27b78d3070a8a8d92ed0d3b96151dca9cb7936
ethanleighfellows <54329697+ethanleighfellows@users.noreply.github.com>
2024-10-29 20:34:17 -0400

    Update app.js

--- script/app.js
+++ script/app.js
@@ -55,9 +55,6 @@ window.addEventListener('load', function() {
     }
 });
 
-// Particles.js Background
-if (document.getElementById('particles-js')) {
-    particlesJS.load('particles-js', 'particles.json', function() {
-        console.log('Particles.js loaded');
-    });
-}
+particlesJS.load('particles-js', 'assets/particles.json', function() {
+  console.log('callback - particles.js config loaded');
+});
